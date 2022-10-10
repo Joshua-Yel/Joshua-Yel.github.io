f2c3313da3b18d84be0dfd4f98d88e1d1d1defc5
Joshua_Tribiana <45102098+Joshua-Yel@users.noreply.github.com>
2022-10-11 00:13:35 +0800

    Blog site

---
+++ Completed/assets/js/main.js
@@ -0,0 +1,87 @@
+// Grab elements
+const selectElement = (selector) => {
+    const element = document.querySelector(selector);
+    if(element) return element;
+    throw new Error(`Something went wrong! Make sure that ${selector} exists/is typed correctly.`);  
+};
+
+// Open menu & search pop-up
+const menuToggleIcon = selectElement('#menu-toggle-icon');
+const formCloseBtn = selectElement('#form-close-btn');
+const searchContainer = selectElement('#search-form-container');
+
+const toggleMenu = () =>{
+    const mobileMenu = selectElement('#menu');
+    mobileMenu.classList.toggle('activated');
+    menuToggleIcon.classList.toggle('activated');
+}
+
+menuToggleIcon.addEventListener('click', toggleMenu);
+
+// Switch theme/add to local storage
+const body = document.body;
+const themeToggleBtn = selectElement('#theme-toggle-btn');
+const currentTheme = localStorage.getItem('currentTheme');
+
+// Check to see if there is a theme preference in local Storage, if so add the ligt theme to the body
+if (currentTheme) {
+    body.classList.add('light-theme');
+}
+
+themeToggleBtn.addEventListener('click', function () {
+    // Add light theme on click
+    body.classList.toggle('light-theme');
+
+    // If the body has the class of light theme then add it to local Storage, if not remove it
+    if (body.classList.contains('light-theme')) {
+        localStorage.setItem('currentTheme', 'themeActive');
+    } else {
+        localStorage.removeItem('currentTheme');
+    }
+});
+
+
+
+//toggle scroll menu
+    var scrollTop = 0;
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        //adjust menu background
+        if (scroll > 80) {
+            if (scroll > scrollTop) {
+                $('.smart-scroll').addClass('scrolling').removeClass('up');
+            } else {
+                $('.smart-scroll').addClass('up');
+            }
+        } else {
+            // remove if scroll = scrollTop
+            $('.smart-scroll').removeClass('scrolling').removeClass('up');
+        }
+
+        scrollTop = scroll;
+
+        // adjust scroll to top
+        if (scroll >= 600) {
+            $('.scroll-top').addClass('active');
+        } else {
+            $('.scroll-top').removeClass('active');
+        }
+        return false;
+    });
+
+    // scroll top top
+    $('.scroll-top').click(function () {
+        $('html, body').stop().animate({
+            scrollTop: 0
+        }, 1000);
+    });
+
+    const btn = document.getElementById("myButton1");
+
+    btn.addEventListener('click', function onClick(event) {
+    document.getElementById("textfield1").value ="";
+    document.getElementById("myButton1").value="Sent ✔";
+    event.target.style.backgroundColor = 'lightgreen';
+    event.target.style.color = 'black';
+});
+    
